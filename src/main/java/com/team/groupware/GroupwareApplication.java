637279261382957fe7af46d6070f89baa9a7c7d2
JaeHong94 <moonshin123@naver.com>
2023-03-23 20:26:23 +0900

    mysql 연동

--- src/main/java/com/team/groupware/GroupwareApplication.java
+++ src/main/java/com/team/groupware/GroupwareApplication.java
@@ -2,8 +2,9 @@ package com.team.groupware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class GroupwareApplication {
 
 	public static void main(String[] args) {
