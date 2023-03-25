94f18901760bf12329db4a8975bf120ac97066b9
JaeHong94 <moonshin123@naver.com>
2023-03-26 03:07:05 +0900

    add: 타임리프 레이아웃 적용

--- src/main/java/com/team/groupware/controller/MainController.java
+++ src/main/java/com/team/groupware/controller/MainController.java
@@ -1,13 +1,15 @@
 package com.team.groupware.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class MainController {
 
   @GetMapping("/")
-  public String homg() {
-    return "home";
+  public String main(Model model) {
+
+    return "main";
   }
 }
