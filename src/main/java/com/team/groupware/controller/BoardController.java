b01509590b6ff7f4a33906b6f9c79c8a396b96ed
JaeHong94 <moonshin123@naver.com>
2023-04-12 17:19:51 +0900

    add: 게시판 리스트 화면 구현

---
+++ src/main/java/com/team/groupware/controller/BoardController.java
@@ -0,0 +1,35 @@
+package com.team.groupware.controller;
+
+import com.team.groupware.entity.Employ;
+import com.team.groupware.repository.EmployRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Slf4j
+@Controller
+@RequestMapping("/board")
+@RequiredArgsConstructor
+public class BoardController {
+
+  private final EmployRepository employRepository;
+
+  @GetMapping("/freeBoard")
+  public String freeBoard(Model model, Principal principal) {
+    Employ employ = employRepository.findByEmpId(principal.getName());
+    model.addAttribute("member", employ);
+    return "board/freeBoard";
+  }
+
+  @GetMapping("/notice")
+  public String notice(Model model, Principal principal) {
+    Employ employ = employRepository.findByEmpId(principal.getName());
+    model.addAttribute("member", employ);
+    return "board/notice";
+  }
+}
